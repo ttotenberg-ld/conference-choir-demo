ec43dc8ea81da10c69c3481aaab5c30ef7e0ab19
Tom Totenberg <ttotenberg@launchdarkly.com>
2023-04-27 16:07:44 -0500

    added more controls

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Sounds from './components/sounds';
+import QR from './components/qrCode'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <QR />
         <Sounds />
       </header>
     </div>
--- src/components/sounds.js
+++ src/components/sounds.js
@@ -2,19 +2,38 @@ import React, { useRef } from 'react';
 import MIDISounds from 'midi-sounds-react';
 import { withLDConsumer } from "launchdarkly-react-client-sdk";
 
-function Sounds ({ ldClient }) {
+function Sounds ({ flags, ldClient }) {
 
-  const pitch = ldClient.variation("config-chord").notes;
+  const chord = ldClient.variation("config-chord").notes;
+
+  const part = ldClient.variation("enable-sound");
+
+  let sound = chord;
+
+  if (part === 'bass') {
+    sound = [chord[0]]
+  } else if (part === 'tenor') {
+    sound = [chord[1]]
+  } else if (part === 'alto') {
+    sound = [chord[2]]
+  } else if (part === 'soprano') {
+    sound = [chord[3]]
+  } else if (part === 'all') {
+    sound = chord
+  } else {
+    sound = null
+  }
+
 
   const midiSoundsRef = useRef();
 
   const playTestInstrument = () => {
-    midiSoundsRef.current.playChordNow(472, [36], 2.5);
+    midiSoundsRef.current.playChordNow(590, sound, 1.5);
   };
 
   return (
       <div className="App">
-          <p className="App-intro">Press Play to play instrument sound.</p>
+          <p className="App-intro">Join the choir!</p>
           <p><button onClick={playTestInstrument}>Play</button></p>
           <MIDISounds ref={midiSoundsRef} appElementName="root" instruments={[3]} />
       </div>
