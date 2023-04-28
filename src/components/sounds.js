f9ae46c26203b8d6a1e6f0be905d4ba19bb3be9c
Tom Totenberg <ttotenberg@launchdarkly.com>
2023-04-28 10:41:57 -0500

    added flag dynamic chords and parts

--- src/components/sounds.js
+++ src/components/sounds.js
@@ -1,41 +1,62 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import MIDISounds from 'midi-sounds-react';
 import { withLDConsumer } from "launchdarkly-react-client-sdk";
 
 function Sounds ({ flags, ldClient }) {
+  const midiSounds = useRef();
 
-  const chord = ldClient.variation("config-chord").notes;
-
-  const part = ldClient.variation("enable-sound");
-
-  let sound = chord;
-
-  if (part === 'bass') {
-    sound = [chord[0]]
-  } else if (part === 'tenor') {
-    sound = [chord[1]]
-  } else if (part === 'alto') {
-    sound = [chord[2]]
-  } else if (part === 'soprano') {
-    sound = [chord[3]]
-  } else if (part === 'all') {
-    sound = chord
-  } else {
-    sound = null
+  function getChord() {
+    const chord = ldClient.variation("config-chord").notes;
+    return chord
   }
+  
+  function getPart() {
+    const part = ldClient.variation("enable-sound");
+    return part
+  }
+  
 
+  let sound = getChord();
+  
+  function getNewSound() {
+    if (getPart() === 'bass') {
+      sound = [getChord()[0]];
+    } else if (getPart() === 'tenor') {
+      sound = [getChord()[1]];
+    } else if (getPart() === 'alto') {
+      sound = [getChord()[2]];
+    } else if (getPart() === 'soprano') {
+      sound = [getChord()[3]];
+    } else if (getPart() === 'all') {
+      sound = getChord();
+    } else {
+      sound = [0];
+    }
+    return sound;
+  }
+  
 
-  const midiSoundsRef = useRef();
+  ldClient.on('change', (settings) => {
+    midiSounds.current.cancelQueue();
+    playTestInstrument();
+  });
 
   const playTestInstrument = () => {
-    midiSoundsRef.current.playChordNow(590, sound, 1.5);
+    midiSounds.current.playChordNow(590, getNewSound(), 9999);
   };
+
+  const stopTestInstrument = () => {
+    midiSounds.current.cancelQueue();
+  }
+
 
   return (
       <div className="App">
           <p className="App-intro">Join the choir!</p>
           <p><button onClick={playTestInstrument}>Play</button></p>
-          <MIDISounds ref={midiSoundsRef} appElementName="root" instruments={[3]} />
+          <p className="App-intro">Stop the choir!</p>
+          <p><button onClick={stopTestInstrument}>Stop!</button></p>
+          <MIDISounds ref={midiSounds} appElementName="root" instruments={[3]} />
       </div>
   );
 }
