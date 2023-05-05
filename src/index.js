730a983093f48c821bf3a85334cfa15258b5fd56
Tom Totenberg <ttotenberg@launchdarkly.com>
2023-05-05 11:31:32 -0500

    more css trickery

--- src/index.js
+++ src/index.js
@@ -13,6 +13,11 @@ const CLIENTKEY = "609ead905193530d7c28647b";
 let deviceKey = getDeviceKey();
 let tableId = getTable();
 
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 (async () => {
   const LDProvider = await asyncWithLDProvider({
     clientSideID: CLIENTKEY,
